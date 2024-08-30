cf7c2bec863872c91f3627425a5aabee5651ca42
4946057 <o.pomarolmoya@uu.nl>
2024-08-30 10:30:09 +0200

    Implemented new changes to forward simulation.

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 # Import libraries
 import time
-import os
-import pandas as pd
-import numpy as np
 from modules.data_preprocessing import data_preprocessing
 from modules.data_preprocessing import data_preprocessing
 from modules.model_training import model_training
 from modules.forward_simulation import forward_simulation
+from modules.simulation_analysis import simulation_analysis
 
 # Record starting run time
 start_time = time.time()
 
-# # Load and preprocess data
-# print('Loading and preprocess the data...')
-# data_preprocessing()
-# print('Successfully loaded and preprocessed the data...')
+# Load and preprocess data
+print('Loading and preprocess the data...')
+data_preprocessing()
+print('Successfully loaded and preprocessed the data...')
 
 # Train the models with the different setups
 print('Training models...')
 model_training()
 print('Successfully trained models...')
 
-# # Test the models by use of the forward simulation
-# print('Performing forward simulation...')
-# forward_simulation(dfs_obs, dfs_mod, dfs_meteo_agg, dfs_mod_delta_swe,
-#                    lag, station_years=['cdp_2002','rme_2002', 'wfj_2002'])
-# print('Successfully performed forward simulation...')
+# Simulate SWE using ML for each station
+print('Performing forward simulation...')
+forward_simulation()
+print('Successfully performed forward simulation...')
 
-# # Print execution time
-# end_time = time.time()
-# execution_time = (end_time - start_time)/60
-# print('Script finalized.\nExecution time: {:.3g} minutes.'.format(execution_time),
-#       '\nEnd time: {}'.format(time.ctime(end_time)))
+# Analyze the simulation results
+print('Analyzing simulation results...')
+simulation_analysis(station_years=['cdp_train', 'rme_train', 'sod_train',
+                                   'cdp_test', 'rme_test', 'sod_test'])
+print('Successfully analyzed simulation results...')
+
+# Print execution time
+end_time = time.time()
+execution_time = (end_time - start_time)/60
+print('Script finalized.\nExecution time: {:.3g} minutes.'.format(execution_time),
+      '\nEnd time: {}'.format(time.ctime(end_time)))
---
+++ modules/simulation_analysis.py
@@ -0,0 +1,149 @@
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from sklearn.metrics import mean_squared_error
+
+def simulation_analysis(station_years=[]):
+    # Define what lag value to use
+    lag = 14
+
+    # List of station names
+    station_names = [
+        "cdp",
+        "oas",
+        "obs",
+        "ojp",
+        "rme",
+        "sap",
+        "snb",
+        "sod",
+        "swa",
+        "wfj",
+    ]
+
+    # Load the station data
+    dict_dfs = {}
+    for station_name in station_names:
+        # Load the obs and mod data
+        dir_path = os.path.join("data", "preprocessed", f"data_daily_lag_{lag}")
+        filename = f"df_{station_name}_lag_{lag}.csv"
+        df_obs = pd.read_csv(os.path.join(dir_path, filename), index_col=0)
+
+        # Subset only the SWE columns
+        df_obs = df_obs[["obs_swe", "mod_swe"]]
+
+        # Load the ML simulated data
+        dir_path = os.path.join("results", "simulated_swe")
+        filename = f"df_{station_name}_sim_swe.csv"
+        df_sim = pd.read_csv(os.path.join(dir_path, filename), index_col=0)
+
+        # Join both dataframes by index
+        df_station = df_obs.join(df_sim, how="outer")
+        df_station.index = pd.to_datetime(df_station.index)
+
+        # Add the data to the dictionary
+        dict_dfs[station_name] = df_station
+
+    # Create an empty dataframe for the nNSE
+    sim_modes = ['mod_swe', 'dir_pred', 'err_corr', 'data_aug']
+    df_nnse = pd.DataFrame(columns=sim_modes)
+
+    # Find the nNSE and store them in the dataframe for each station
+    for station_name in station_names:
+
+        # Clean the data
+        df_station_clean = dict_dfs[station_name].dropna()
+        
+        # Calculate the nNSE of the whole station and append it to the df
+        obs_swe_train = df_station_clean['obs_swe']
+        nnse_train = [calculate_nNSE(obs_swe_train, df_station_clean[mode]) for mode in sim_modes]
+        df_nnse.loc[station_name] = nnse_train
+
+    # Save the nNSE as csv
+    df_nnse.to_csv(os.path.join('results', 'fwd_sim_nnse.csv'))
+
+    # Plot the results
+    print('Plotting the results...')  
+    for station_year in station_years:
+
+        station_name, year = station_year.split("_")
+
+        # Initialize the figure depending on the number of stations
+        if station_name == 'all':
+            station_names_plot = station_names
+            fig, axs = plt.subplots(5, 2, figsize=(20, 25))
+            axs = axs.flatten()
+        
+        else:
+            station_names_plot = [station_name]
+            fig, axs = plt.subplots(1, 1, figsize=(20, 7))
+            axs = [axs]
+
+        # Create a dictionary with the dataframes to plot, and a list of labels
+        dict_dfs_plot = {st_name: dict_dfs[st_name] for st_name in station_names_plot}
+        labels = []
+
+        for station_idx, (station_name, df_station) in enumerate(dict_dfs_plot.items()):
+            
+            # Get the axis
+            ax = axs[station_idx]
+
+            # Mask the measurements and find the number of data points
+            df_masked = mask_measurements_by_year(df_station, year)
+            df_masked_clean = df_masked.dropna()
+            num_data_points = df_masked_clean['obs_swe'].count()
+
+            # Plot the observed SWE and calculate the nNSE
+            for column_name in df_masked.columns:
+                nNSE = calculate_nNSE(df_masked_clean["obs_swe"] ,
+                                      df_masked_clean[column_name])
+                clean_column = df_masked[column_name].dropna()                
+                ax.plot(clean_column.index, clean_column,
+                        label=f'{column_name} (nNSE: {nNSE:.2f})')
+
+            # Create the  legend
+            ax.legend(fontsize='large')
+            handles, labels = ax.get_legend_handles_labels()
+            handles.append(Line2D([0], [0], marker='None', color='white', label=f'Data points: {num_data_points}'))
+            labels.append(f'Data points: {num_data_points}')
+            ax.legend(handles=handles, labels=labels, fontsize='large')
+
+            ax.set_xlabel('Date')
+            ax.set_ylabel('SWE')
+            ax.set_title(f'{station_name.upper()} {year}')
+            plt.savefig(os.path.join('results', f'fwd_sim_{station_name}_{year}.png'))
+
+###############################################################################
+# EXTRA FUNCTIONS
+###############################################################################
+
+def mask_measurements_by_year(df, year, split_dates=None):
+
+    if year == 'all':
+        return df
+    
+    elif year == 'train':
+        start_date, end_date = split_dates
+        mask = (df.index < start_date) | (df.index > end_date)
+
+    elif year == 'test':
+        start_date, end_date = split_dates
+        mask = (df.index >= start_date) & (df.index <= end_date)
+
+    elif year.isdigit(): 
+        year = int(year)
+        start_date = pd.to_datetime(f'{year}-07-01')
+        end_date = pd.to_datetime(f'{year + 1}-07-01')
+        mask = (df.index >= start_date) & (df.index < end_date)
+        
+    else:
+        raise ValueError(f'Invalid input year: {year}')
+    
+    return df[mask]
+    
+def calculate_nNSE(observed, predicted):
+    mse = mean_squared_error(observed, predicted)
+    nNSE = 1 / (2 - (1 - mse / np.var(observed)))
+    return nNSE
