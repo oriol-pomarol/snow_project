38700fe4418c65ec0bff02ae9ace69ca70cc7aaa
oriol-pomarol <oriol.pomarol@gmail.com>
2024-11-07 11:21:44 +0100

    Changed modified variables to lag and temporal_split, automatize results directories, and config is written to outputs.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import time
-import numpy as np
 from modules.data_processing import data_processing
 from modules.model_selection import model_selection
 from modules.model_training import model_training
@@ -10,25 +9,18 @@ from config import cfg, paths
 def generate_configs():
     
     # Define the values for the configuration parameters
-    rel_weight_values = np.linspace(0.5, 2.0, 3)
     temporal_split_values = [True, False]
+    lag_values = [0, 14]
 
     # Generate the configurations and result paths
-    configs = [cfg(rel_weight=(rw,), temporal_split=ts) \
-               for rw in rel_weight_values for ts in temporal_split_values]
-    config_names = [f"rw_{config.rel_weight}_ts_{config.temporal_split}" for config in configs]
+    configs = [cfg(temporal_split=ts, lag=lg) for ts in temporal_split_values for lg in lag_values]
+    config_names = [f"ts_{config.temporal_split}_lg_{config.lag}" for config in configs]
     result_paths = [paths(results = paths.root / "results" / name) for name in config_names]
-
-    # Create the result directories
-    for path in result_paths:
-        path.results.mkdir(parents=True, exist_ok=True)
-        # Create the subdirectories
-        for sub_dir in ['models', 'figures', 'outputs']:
-            (path.results / sub_dir).mkdir(parents=True, exist_ok=True)
 
     return configs, result_paths
 
 def run_with_config(config, result_path):
+
     # Override the global cfg and paths with the new values
     global cfg, paths
     cfg = config
@@ -67,7 +59,18 @@ def run_with_config(config, result_path):
     execution_time = (end_time - start_time) / 60
     print('Script finalized.\nExecution time: {:.3g} minutes.'.format(execution_time))
 
-# Run the code with the specified configurations and result paths
+# Generate the configurations and result paths
 configs, result_paths = generate_configs()
+
 for config, result_path in zip(configs, result_paths):
+
+    # Create the results directories if missing
+    for sub_dir in ['models', 'figures', 'outputs']:
+        (result_paths / sub_dir).mkdir(parents=True, exist_ok=True)
+
+    # Save the configuration to a file in the outputs directory
+    with open(result_path / 'config.txt', 'w') as f:
+        f.write(str(config))
+        
+    # Run the code
     run_with_config(config, result_path)
