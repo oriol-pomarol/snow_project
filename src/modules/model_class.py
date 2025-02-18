42e3e3fa3b47476400135ab995f386ea55846d45
oriol-pomarol <oriol.pomarol@gmail.com>
2025-02-18 16:50:09 +0100

    Added description of the class methods.

--- src/modules/model_class.py
+++ src/modules/model_class.py
@@ -14,6 +14,12 @@ from .auxiliary_functions import preprocess_data_lstm
 
 class Model:
     def __init__(self, mode):
+        """
+        Initialize the Model class.
+
+        Parameters:
+        mode (str): The mode of the model (e.g., 'dir_pred', 'post_prc').
+        """
         self.mode = mode.lower()
         self.model = None
         self.model_type = None
@@ -23,11 +29,25 @@ class Model:
         self.best_epochs = []
 
     def set_hps(self, model_type, hyperparameters, epochs=None):
+        """
+        Set the hyperparameters for the model.
+
+        Parameters:
+        model_type (str): The type of the model ('nn', 'lstm' or 'rf').
+        hyperparameters (dict): The hyperparameters for the model.
+        epochs (list, optional): The epochs at which to save the model.
+        """
         self.model_type = model_type.lower()
         self.hyperparameters = hyperparameters
         self.epochs = epochs
     
     def save_hps(self, path_dir=None):
+        """
+        Save the hyperparameters to a JSON file.
+
+        Parameters:
+        path_dir (Path, optional): The directory to save the hyperparameters file.
+        """
         if path_dir is None:
             path_dir = paths.temp
         hps_mt = {
@@ -40,6 +60,12 @@ class Model:
             json.dump(hps_mt, f)
 
     def load_hps(self, path_dir=None):
+        """
+        Load the hyperparameters from a JSON file.
+
+        Parameters:
+        path_dir (Path, optional): The directory to load the hyperparameters file from.
+        """
         if path_dir is None:
             path_dir = paths.temp
         with open(path_dir / f'{self.mode}_hps.json', 'r') as f:
@@ -50,8 +76,17 @@ class Model:
             self.rel_weight = hps_mt.get('rel_weight')
 
     def create_model(self, input_shape, n_met_vars):
-        self.model = None  # Clear any existing model
+        """
+        Create the model based on the stored type and hyperparameters.
 
+        Parameters:
+        input_shape (tuple): The shape of the input data.
+        n_met_vars (int): The number of meteorological variables.
+        """
+        # Clear any existing model
+        self.model = None 
+
+        # If the model type is a neural network, create a keras model accordingly
         if self.model_type == 'nn':
             self.model = keras.Sequential()
             self.model.add(keras.layers.Input(shape=input_shape))
@@ -62,6 +97,7 @@ class Model:
             self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.hyperparameters.get('learning_rate', 0.001)),
                                loss='mean_squared_error', metrics=['mean_squared_error'], weighted_metrics=[])
         
+        # If the model type is an lstm, create a keras model accordingly
         elif self.model_type == 'lstm':
             sequential_input = keras.layers.Input(shape=(cfg.lag, n_met_vars // cfg.lag))
             activation = self.hyperparameters.get('activation', 'relu')
@@ -84,13 +120,19 @@ class Model:
             self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.hyperparameters.get('learning_rate', 0.001)),
                                loss='mean_squared_error', metrics=['mean_squared_error'], weighted_metrics=[])
         
+        # If the model type is a random forest, create an sklearn model accordingly
         elif self.model_type == 'rf':
             self.model = RandomForestRegressor(n_estimators=200, random_state=10,
                                                max_depth=self.hyperparameters.get('max_depth', None),
                                                max_samples=self.hyperparameters.get('max_samples', None))
-            
-    def save_model(self, suffix=''):
 
+    def save_model(self, suffix=''):
+        """
+        Save the model to a joblib or h5 file.
+
+        Parameters:
+        suffix (str, optional): The suffix to add to the model name.
+        """
         # Include the suffix (if provided) to the mode to generate the model name
         model_name = '_'.join([self.mode, suffix]) if suffix else self.mode
 
@@ -105,7 +147,12 @@ class Model:
 
     
     def load_model(self, suffix=''):
+        """
+        Load the model from a file.
 
+        Parameters:
+        suffix (str, optional): The suffix to add to the model name.
+        """
         # Include the suffix (if provided) to the mode to generate the model name
         model_name = '_'.join([self.mode, suffix]) if suffix else self.mode
 
@@ -120,7 +167,15 @@ class Model:
 
 
     def fit(self, X, y, save_train_history=False, **kwargs):
+        """
+        Fit the model to the training data.
 
+        Parameters:
+        X (DataFrame): The input features.
+        y (Series): The target values.
+        save_train_history (bool, optional): Whether to save the training history.
+        kwargs: Additional arguments for the fit method.
+        """
         # Drop a percentage of the data if specified
         if cfg.drop_data > 0:
 
@@ -187,7 +242,15 @@ class Model:
         return
 
     def predict(self, X):
+        """
+        Predict the target values for the input features.
 
+        Parameters:
+        X (DataFrame): The input features.
+
+        Returns:
+        np.array: The predicted target values.
+        """
         # If the dataset is empty, return an empty array
         if len(X) == 0:
             return np.array([])
@@ -211,7 +274,16 @@ class Model:
         return y_pred
 
     def test(self, X, y):
+        """
+        Test the model on the input features and target values.
 
+        Parameters:
+        X (DataFrame): The input features.
+        y (Series): The target values.
+
+        Returns:
+        float: The mean squared error of the predictions.
+        """
         # Drop a percentage of the data if specified
         if cfg.drop_data > 0:
 
@@ -251,10 +323,25 @@ class Model:
         return mse
 
     def get_model_type(self):
+        """
+        Get the type of the model.
+
+        Returns:
+        str: The type of the model.
+        """
         return self.model_type
 
     def __str__(self):
+        """
+        Get the string representation of the model type and hyperparameters.
+
+        Returns:
+        str: The string representation of the model.
+        """
+        # Initialize the model name with the model type
         model_name = self.model_type
+
+        # Add the hyperparameters to the model name
         for key, value in self.hyperparameters.items():
 
             # Take the first two characters of the hyperparameter name
@@ -272,21 +359,39 @@ class Model:
                 param_name = 'rg'
             else:
                 value_str = str(value)
+
+            # Add the hyperparameters to the model name
             model_name += f"_{param_name}_{value_str}"
-        # Add the relative weight to the model name if not None
+
+        # Add the relative weight to the model name, if existant
         if self.rel_weight is not None:
             model_name += f"_rw_{self.rel_weight:.0e}".replace("-", "_")
             
         return model_name
-    
+
+###############################################################################
+
 # Define a custom callback to save models at specific epochs
 class SaveModelAtEpoch(Callback):
     def __init__(self, save_epochs):
+        """
+        Initialize the SaveModelAtEpoch callback.
+
+        Parameters:
+        save_epochs (list): The epochs at which to save the model.
+        """
         super(SaveModelAtEpoch, self).__init__()
         self.save_epochs = save_epochs
         self.saved_models = {}
 
     def on_epoch_end(self, epoch, logs=None):
+        """
+        Save the model at the specified epochs.
+
+        Parameters:
+        epoch (int): The current epoch.
+        logs (dict): The logs of the model.
+        """
         if (epoch + 1) in self.save_epochs:
             # Clone the model and store it
             model_copy = tf.keras.models.clone_model(self.model)
@@ -295,4 +400,10 @@ class SaveModelAtEpoch(Callback):
             print(f"\n\nModel saved at epoch {epoch + 1}\n")
 
     def get_saved_models(self):
+        """
+        Get the saved models.
+
+        Returns:
+        dict: The saved models.
+        """
         return self.saved_models
